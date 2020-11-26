2c98262241935a589542926fde837da3e9e85230
AlexKalashnikov <rombik_84@mail.ru>
2020-11-26 17:57:25 +0300

    "Initial commit"

---
+++ routes/users.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET userlist. */
+router.get('/userlist', function(req, res) {
+  let db = req.db;
+  let collection = db.get('userlist');
+  collection.find({},{},function(e,docs){
+    res.json(docs);
+  });
+});
+
+/* POST to adduser. */
+router.post('/adduser', function(req, res) {
+  let db = req.db;
+  let collection = db.get('userlist');
+  collection.insert(req.body, function(err, result){
+    res.send(
+      (err === null) ? { msg: '' } : { msg: err }
+    );
+  });
+});
+
+/* DELETE to deleteuser. */
+router.delete('/deleteuser/:id', function(req, res) {
+  let db = req.db;
+  let collection = db.get('userlist');
+  let userToDelete = req.params.id;
+  collection.remove({ '_id' : userToDelete }, function(err) {
+    res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+  });
+});
+
+module.exports = router;
